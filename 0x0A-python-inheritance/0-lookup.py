1d7c36543247706238a30f08a3ac48555b1c0b97
Webi Muleta <webimuleta01@gmail.com>
2023-11-06 20:39:40 +0300

    Project files

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """Defines an object attribute lookup function."""
+
+
 def lookup(obj):
     """
     Returns a list of valid attributes and methods for the given object.
-
-    :param obj: The object to look up.
-    :type obj: Any
-    :return: A list of valid attributes and methods for the object.
-    :rtype: List[str]
     """
     return (dir(obj))
