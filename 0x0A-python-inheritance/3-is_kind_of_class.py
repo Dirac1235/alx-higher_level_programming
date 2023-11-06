80ed73c8863092d601a7543856dee97aec47af77
Webi Muleta <webimuleta01@gmail.com>
2023-11-06 21:48:33 +0300

    Project file

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """Defines a class-checking function."""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Check if an object is exactly an instance of a given class.
 
     Args:
@@ -12,6 +12,6 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    if type(obj) == a_class:
+    if issubclass(type(obj), a_class):
         return True
     return False
