af14b340bc4351d19db977372db3148b7293f30e
Webi Muleta <webimuleta01@gmail.com>
2023-11-07 20:43:49 +0300

    Project file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ function that reads from file and prints to stdout"""
     with open(filename) as file:
-        print(file.read())
+        print(file.read(), end="")
