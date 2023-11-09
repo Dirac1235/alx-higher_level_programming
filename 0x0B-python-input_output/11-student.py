3fb74559523bbcdb54ca8d7494b8809c5849a443
Webi Muleta <webimuleta01@gmail.com>
2023-11-09 04:37:59 +0300

    Project file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): First name of student.
+            last_name (str): Last name of student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=[]):
+        """Get a dictionary representation of the Student."""
+        diction = self.__dict__
+        if attr == []:
+            return diction
+
+        def my_filter_fun(pair):
+            """used to filter the dictionary"""
+            keys, value = pair
+            return keys in attr
+        diction = dict(filter(my_filter_fun, diction.items()))
+        return diction
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
