8862f4155160a9604dc29864fa7f0e0ba7b23d0f
Webi Muleta <webimuleta01@gmail.com>
2023-11-17 19:38:18 +0300

    Project files

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,12 @@
+This folder contains necassary projects that help us for the AirBnB project
+It contains review for:
+   	Import
+   	Exceptions
+	Class
+	Private attribute
+	Getter/Setter
+	Class method
+      	Static method
+       	Inheritance
+	Unittest
+	Read/Write file
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" contains base class """
+
+
+class Base(object):
+    """ contains a Base class modules """
+
+    __nb_objects = 0
+    id = 0
+
+    def __init__(self, id=None):
+        """ initalizes id"""
+        if id != None:
+            self.id = id
+        else:
+            Base.id+=1
+
+    __nb_objects+=1
