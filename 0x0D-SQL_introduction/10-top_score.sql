2c2f056bc68a38bf29423d1dda56059e98ee3ff9
Webi Muleta <webimuleta01@gmail.com>
2023-12-14 13:51:42 +0300

    project file

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Lists the records inside the table second_table based on the score
+SELECT `score`, `name` FROM `second_table` 
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- shows teh full description of the tables
+SHOW CREATE TABLE `first_table`;
