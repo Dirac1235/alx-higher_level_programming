7635aeebcd4d253b8241e921364d8cfc7bdea6b0
Webi Muleta <webimuleta01@gmail.com>
2023-12-15 05:26:44 +0300

    Project file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Creates user for MYSQL server user_0d_1
 CREATE  USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL ON *.*
-TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
