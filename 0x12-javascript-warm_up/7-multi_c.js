bba433484007381995a150808c65af5cb11cf262
Webi Muleta <webimuleta01@gmail.com>
2024-01-08 22:11:41 +0300

    Project file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-const x = process.argv[2];
-for (let i = 0; i < x; i++) {
-  console.log('C is fun');
-}
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+	console.log('Missing number of occurrences');
+  } else {
+	const x = Number(process.argv[2]);
+	let i = 0;
+	while (i < x) {
+	  console.log('C is fun');
+	  i++;
+	}
+  }
+  
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,12 @@
-#!/us/bin/node
-const x = process.argv[2];
-let y = '';
-
-for (let i = 0; i < x; i++) {
-  y += '#';
-}
-for (let i = 0; i < x; i++) {
-  console.log(y);
-}
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+	console.log('Missing size');
+  } else {
+	const x = Number(process.argv[2]);
+	let i = 0;
+	while (i < x) {
+	  console.log('X'.repeat(x));
+	  i++;
+	}
+  }
+  
