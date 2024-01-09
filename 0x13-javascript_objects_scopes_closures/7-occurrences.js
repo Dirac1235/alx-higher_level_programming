4b343954c329fbe5dd202188782350462fcc49e5
Webi Muleta <webimuleta01@gmail.com>
2024-01-09 19:43:28 +0300

    project file

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    list.forEach(element => {
-      if (element === searchElement)  {
-        count += 1;
-      }
-    });
-    return count;
-}
+  let count = 0;
+  list.forEach(element => {
+    if (element === searchElement) {
+      count += 1;
+    }
+  });
+  return count;
+};
