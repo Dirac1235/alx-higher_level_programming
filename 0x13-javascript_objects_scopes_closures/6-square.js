b6cde70010d0c138191a3ffd0cc74a91bc9452c7
Webi Muleta <webimuleta01@gmail.com>
2024-01-09 19:48:52 +0300

    Project file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const Squarep = require('./5-square.js');
+class Square extends Squarep {
   constructor (size) {
     super(size, size);
   }
