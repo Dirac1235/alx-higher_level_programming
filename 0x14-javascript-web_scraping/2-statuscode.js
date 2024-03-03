5b9a00fc4e3ce94ff2db77378d66b0392c7b4ead
Webi Muleta <webimuleta01@gmail.com>
2024-03-03 10:03:24 +0300

    Project file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 
 const request = require('request');
 
-request(process.argv[2], function (error, response) {
-  console.log('code:', response.statusCode); // Print the response status code if a response was received
+request(process.argv[2], function (_error, response) {
+  console.log('code:', response.statusCode);
 });
