9d025d50f0e4b5176c2374ba0f381915b7568727
Webi Muleta <webimuleta01@gmail.com>
2024-03-03 10:14:26 +0300

    making the files to confom to semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const star_wars_uri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(star_wars_uri, function (_err, _res, body) {
+request(starWarsUri, function (_err, _res, body) {
   body = JSON.parse(body);
   console.log(body.title);
 });
